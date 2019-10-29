867847f41937b50dec23d60ffa4c2a142fd24e67
石川 博和 <ishikawa@ishikawanoMacBook-Pro.local>
2019-10-29 11:35:53 +0900

    pull-request

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello java.");
 		System.out.println("first pull-request.");
+		System.out.println("edit-readme.");
 	}
 }
